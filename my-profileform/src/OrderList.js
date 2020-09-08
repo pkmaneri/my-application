e6b0a9672472ebc2884df590372f15285896d567
praveen.kumar <praveen.kumar.maneri@gmail.com>
2020-09-08 17:22:33 +0530

    add todo list

--- my-profileform/src/OrderList.js
+++ my-profileform/src/OrderList.js
@@ -3,20 +3,39 @@ import Order from "./Order"
 
 
 export default class OrderList extends React.Component {
-    state ={
-        values:["a","sa"]
+    state = {
+        currentValue: '',
+        values: []
+    }
+
+    handleChange(e) {
+        const value = e.target.value;
+        this.setState(prevState => {
+            prevState.currentValue = value;
+            return prevState
+        })
+    }
+
+    handleClick(e) {
+        this.setState(prevState => {
+            prevState.values.push(prevState.currentValue);
+            return prevState;
+        })
     }
     render() {
         return (
-            <ol>
-                {this.state.values.map((value) =>{
-                    return (
-                        <Order value={value}/>
-                    )
-                })}
+            <>
+                <input onChange={this.handleChange.bind(this)} />
+                <button onClick={this.handleClick.bind(this)}>Add</button>
+                <ol>
+                    {this.state.values.map((value) => {
+                        return (
+                            <Order value={value} />
+                        )
+                    })}
 
-            </ol>
-          
+                </ol>
+            </>
         )
     }
 }
